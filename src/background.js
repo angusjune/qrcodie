3ae868d9b5f964914702cc85034fc87362f65c0a
angusjune <angus.is.online@gmail.com>
2021-04-20 15:33:14 +0800

    Fixed a bug when updating browser icon may cause it to be blurry

--- src/background.js
+++ src/background.js
@@ -4,14 +4,14 @@ const isBrowserDark  = window.matchMedia("(prefers-color-scheme: dark)").matches
 
 if (isBrowserDark) {
     // set light-colored icon in dark UI
-    chrome.browserAction.setIcon({ path: `icons/icon-light-32.png`});
+    chrome.browserAction.setIcon({ path: `icons/icon-light-16.png`});
 }
 
 chrome.runtime.onMessage.addListener(message => {
     if (message.isBrowserDark) {
-        chrome.browserAction.setIcon({ path: `icons/icon-light-32.png`});
+        chrome.browserAction.setIcon({ path: `icons/icon-light-16.png`});
     } else {
-        chrome.browserAction.setIcon({ path: `icons/icon-32.png`});
+        chrome.browserAction.setIcon({ path: `icons/icon-16.png`});
     }
     console.log(message);
 });
